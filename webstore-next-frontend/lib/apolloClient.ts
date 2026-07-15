5f5758bd8db1a614a753eab9fa241c7bf8e19028
Kaday Hein <kadayhein@Kadays-MacBook-Air.local>
2026-07-15 19:18:33 +0900

    graphiql url changed

--- webstore-next-frontend/lib/apolloClient.ts
+++ webstore-next-frontend/lib/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "https://fashion-storehub.onrender.com/graphiql",
+    uri: "https://fashion-storehub.onrender.com/graphiql?path=/graphql",
   }),
   cache: new InMemoryCache(),
 });
