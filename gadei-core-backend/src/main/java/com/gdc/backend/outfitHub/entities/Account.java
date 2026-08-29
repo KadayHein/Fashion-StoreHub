e466a91f15fb5eb46c1164d45e7e6de46bd7eff9
Kaday Hein <kadayhein@Kadays-MacBook-Air.local>
2026-08-29 19:30:18 +0900

    Admin Panel+ Globalization+ Filter/Search+ Checkout Details+ Responsive UI

--- gadei-core-backend/src/main/java/com/gdc/backend/outfitHub/entities/Account.java
+++ gadei-core-backend/src/main/java/com/gdc/backend/outfitHub/entities/Account.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,32 +16,39 @@ import java.time.LocalDateTime;
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name = "account")
 public class Account {
+
     @Id
     private String email;
 
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(name = "full_name", nullable = false)
     private String fullName;
 
-    @Column(nullable = false)
-    @NumberFormat(style = NumberFormat.Style.NUMBER, pattern = "###-###-###")
+    @Column(
+        name = "phone_number",
+        nullable = false
+    )
     private String phoneNumber;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role = Role.ROLE_USER;
 
-    @Column(nullable = false, updatable = false)
+    @Column(
+        name = "created_at",
+        nullable = false,
+        updatable = false
+    )
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    public enum Role{
+    public enum Role {
         ROLE_USER,
         ROLE_ADMIN
-    }
-    
+    }    
     
 
     public Account(String email, String password, String fullname, String phoneNumber){
